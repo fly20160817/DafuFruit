3d7bab2e68f5f0995ed893e01ed03c38424a356f
方冬雷 <724310585@qq.com>
2016-10-17 19:32:53 +0800

    无错回档

---
+++ DafuFruit/DafuFruit/TabViewController.m
@@ -0,0 +1,45 @@
+//
+//  TabViewController.m
+//  DafuFruit
+//
+//  Created by 方冬雷 on 16/10/14.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "TabViewController.h"
+
+@interface TabViewController ()
+
+@end
+
+@implementation TabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UINavigationController *HomeNC = [[UINavigationController alloc]initWithRootViewController:[[UIStoryboard storyboardWithName:@"FDL" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"Home"] ];
+    HomeNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"滴滴鲜" image:[UIImage imageNamed:@"lemon"] tag:0];
+    UINavigationController *PersonNC = [[UINavigationController alloc]initWithRootViewController:[[UIStoryboard storyboardWithName:@"FDL" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"Person"] ];
+    PersonNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"尝鲜人" image:[UIImage imageNamed:@"person"] tag:0];
+    
+    
+    self.viewControllers = @[HomeNC, PersonNC];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
