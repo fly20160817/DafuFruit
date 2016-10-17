77b70c39ff05396697ae58f873e6ce1b606bb6ab
方冬雷 <724310585@qq.com>
2016-10-17 10:25:47 +0800

    滚动视图

--- DafuFruit/DafuFruit/HomeTableViewController.m
+++ DafuFruit/DafuFruit/HomeTableViewController.m
@@ -8,8 +8,16 @@
 
 #import "HomeTableViewController.h"
 #import <CoreLocation/CoreLocation.h>
+#define KOUNT 3
 @interface HomeTableViewController ()<CLLocationManagerDelegate, UIScrollViewDelegate>
 @property (strong, nonatomic) CLLocationManager *locationManager;//定位器
+@property (nonatomic,weak) UIScrollView *scrollView;
+@property (nonatomic,weak) UIImageView *currentImageView;   // 当前imageView
+@property (nonatomic,weak) UIImageView *nextImageView;      // 下一个imageView
+@property (nonatomic,weak) UIImageView *preImageView;       //上一个imageView
+@property (nonatomic,assign) BOOL isDragging;               //是否正在拖动
+@property (nonatomic,strong)NSTimer *timer;                 //设置动画
+//@property (nonatomic, readonly) NSInteger *KOUNT;
 @end
 
 @implementation HomeTableViewController
@@ -132,33 +140,136 @@
 
 -(void)setHeaderView{
     UIScrollView *scrollView =[[UIScrollView alloc] init];
-    scrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    self.scrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
     [self.tableView addSubview:scrollView];
-    [scrollView setContentSize:CGSizeMake(self.view.frame.size.width * 3, self.view.frame.size.height)];
-    self.tableView.tableHeaderView = scrollView;
+    self.scrollView = scrollView;
+    [self.scrollView setContentSize:CGSizeMake(self.view.frame.size.width * 3, self.view.frame.size.height)];
+    //  设置隐藏横向条
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    //  设置自动分页
+    self.scrollView.pagingEnabled = YES;
+    //  设置代理
+    self.scrollView.delegate = self;
+    //  设置当前点
+    self.scrollView.contentOffset = CGPointMake(self.view.frame.size.width, 0);
+    //  设置是否有边界
+    self.scrollView.bounces = NO;
+    //  初始化当前视图
+    UIImageView *currentImageView =[[UIImageView alloc] init];
+    currentImageView.image = [UIImage imageNamed:@"1.jpg"];
+    [self.scrollView addSubview:currentImageView];
+    self.currentImageView = currentImageView;
+    self.currentImageView.frame = CGRectMake(self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.width);
+    self.currentImageView.contentMode = UIViewContentModeScaleAspectFill;
+    //  初始化下一个视图
+    UIImageView *nextImageView = [[UIImageView alloc] init];
+    nextImageView.image = [UIImage imageNamed:@"2.jpg"];
+    [self.scrollView addSubview:nextImageView];
+    self.nextImageView = nextImageView;
+    self.nextImageView.frame = CGRectMake(self.view.frame.size.width * 2, 0, self.view.frame.size.width, self.view.frame.size.height);
+    self.nextImageView.contentMode = UIViewContentModeScaleAspectFill;
+    //  初始化上一个视图
+    UIImageView *preImageView =[[UIImageView alloc] init];
+    preImageView.image = [UIImage imageNamed:@"3.jpg"];
+    preImageView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.scrollView addSubview:preImageView];
+    self.preImageView = preImageView;
+    self.preImageView.contentMode =UIViewContentModeScaleAspectFill;
+    
+    //  设置时钟动画 定时器
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(update:) userInfo:nil repeats:YES];
+    //  将定时器添加到主线程
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    self.tableView.tableHeaderView = self.scrollView;
 }
 
+- (void)update:(NSTimer *)timer{
+    //定时移动
+    
+    if (_isDragging == YES) {
+        return ;
+    }
+    CGPoint offSet = self.scrollView.contentOffset;
+    offSet.x +=offSet.x;
+    [self.scrollView setContentOffset:offSet animated:YES];
+    
+    if (offSet.x >= self.view.frame.size.width *2) {
+        offSet.x = self.view.frame.size.width;
+    }
+    
+    
+}
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    _isDragging = YES;
+}
+//  停止滚动
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    _isDragging = NO;
+    //step = 0;
+}
+
+// 开始拖动
+- (void) scrollViewDidScroll:(UIScrollView *)scrollView{
+    static int i =1; //   当前展示的是第几张图片
+    float offset = self.scrollView.contentOffset.x;
+    if (self.nextImageView.image == nil || self.preImageView.image == nil) {
+        //  加载下一个视图
+        NSString *imageName1 = [NSString stringWithFormat:@"%d.jpg",i == KOUNT ? 1:i +1];
+        _nextImageView.image = [UIImage imageNamed:imageName1];
+        // 加载上一个视图
+        NSString *imageName2 = [NSString stringWithFormat:@"%d.jpg",i==1 ? KOUNT :i-1];
+        _preImageView.image = [UIImage imageNamed:imageName2];
+    }
+    if(offset ==0){
+        _currentImageView.image = _preImageView.image;
+        scrollView.contentOffset = CGPointMake(scrollView.bounds.size.width, 0);
+        _preImageView.image = nil;
+        if (i == 1) {
+            i =KOUNT;
+        } else{
+            i-=1;
+        }
+    }
+    if (offset == scrollView.bounds.size.width * 2) {
+        _currentImageView.image = _nextImageView.image;
+        scrollView.contentOffset = CGPointMake(scrollView.bounds.size.width, 0);
+        _nextImageView.image = nil;
+        if (i == KOUNT) {
+            i  =1 ;
+        }else{
+            i +=1 ;
+        }
+    }
+    
+}
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 0;
+    return 1;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" ];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
