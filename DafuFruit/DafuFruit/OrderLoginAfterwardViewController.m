a0b3876be71a07ac3601145eaea8eb3ec0db3b3c
fly <809176334@qq.com>
2016-10-18 14:28:06 +0800

    未登录订单

---
+++ DafuFruit/DafuFruit/OrderLoginAfterwardViewController.m
@@ -0,0 +1,37 @@
+//
+//  OrderLoginAfterwardViewController.m
+//  DafuFruit
+//
+//  Created by admin on 16/10/18.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "OrderLoginAfterwardViewController.h"
+
+@interface OrderLoginAfterwardViewController ()
+
+@end
+
+@implementation OrderLoginAfterwardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
