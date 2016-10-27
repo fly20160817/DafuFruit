36417c9191a684923656ff0696873643ff6a7551
fly <809176334@qq.com>
2016-10-28 00:21:46 +0800

    支付

---
+++ DafuFruit/DafuFruit/LonginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LonginViewController.h
+//  DafuFruit
+//
+//  Created by admin on 16/10/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LonginViewController : UIViewController
+
+@end
---
+++ DafuFruit/DafuFruit/OrderLoginAfterwardTableViewCell.h
@@ -0,0 +1,23 @@
+//
+//  OrderLoginAfterwardTableViewCell.h
+//  DafuFruit
+//
+//  Created by admin on 16/10/18.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderLoginAfterwardTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *Image;
+
+@property (weak, nonatomic) IBOutlet UILabel *fruitName;
+
+@property (weak, nonatomic) IBOutlet UILabel *OrderDetails;
+
+@property (weak, nonatomic) IBOutlet UILabel *orderNumber;
+
+
+
+@end
